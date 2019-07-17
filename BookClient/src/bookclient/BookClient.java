412e677a3b699dab3eb2879f22a922ff8a77dfc6
NINadjem <nadjemni@gmail.com>
2019-07-18 00:13:10 +0100

    *

--- BookClient/src/bookclient/BookClient.java
+++ BookClient/src/bookclient/BookClient.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,10 +21,9 @@ public class BookClient extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
-        
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
